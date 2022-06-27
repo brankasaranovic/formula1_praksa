21cc0605758982409a8f21515dc035b61b105e37
Branka <saranovicbranka@gmail.com>
2022-06-27 10:32:50 +0200

    Init project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; //export default
+import "./css/main.css";
+
+
+
+
+
+var root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App />);
+
