0f6f0b03b2ecc02551ec511ecb6e478250ac37bf
Sladja90 <vukovsladja@yahoo.com>
2022-06-28 17:51:58 +0200

    Drivers table css stilizovanje 1

--- src/components/DriverDetails.js
+++ src/components/DriverDetails.js
@@ -46,26 +46,28 @@ export default class DriverDetails extends React.Component {
             <div className="driver-details">
 
                 {/* leva tabela */}
-                <div className="driver-personal-details">
-                    <table>
-                        <tbody>
-                            <tr><td colSpan={2}>{this.state.driverDetails.Driver.givenName} {this.state.driverDetails.Driver.familyName}</td></tr>
-                            <tr><td>Country:</td><td>{this.state.driverDetails.Driver.nationality}</td></tr>
-                            <tr><td>Team:</td><td>{this.state.driverDetails.Constructors[0].name}</td></tr>
-                            <tr><td>Birth:</td><td>{this.state.driverDetails.Driver.dateOfBirth}</td></tr>
-                            <tr><td>Biography:</td><td><a href={this.state.driverDetails.Driver.url}>Wiki</a></td></tr>
-                        </tbody>
-                    </table>
+                <div className="driver-personal-details-div">
+                    <div className="driver-personal-details-table">
+                        <table>
+                            <tbody>
+                                <tr><td colSpan={2}>{this.state.driverDetails.Driver.givenName} {this.state.driverDetails.Driver.familyName}</td></tr>
+                                <tr><td>Country:</td><td>{this.state.driverDetails.Driver.nationality}</td></tr>
+                                <tr><td>Team:</td><td>{this.state.driverDetails.Constructors[0].name}</td></tr>
+                                <tr><td>Birth:</td><td>{this.state.driverDetails.Driver.dateOfBirth}</td></tr>
+                                <tr><td>Biography:</td><td><a href={this.state.driverDetails.Driver.url}>Wiki</a></td></tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
                 {/* desna tabela */}
-                <div className="driver-race-details">
-                    <table>
+                <div className="driver-race-details-div">
+                    <table className="driver-race-details-table">
                         <thead>
-                            <tr>
+                            <tr className="raceTable-headerUp">
                                 <td colSpan={5}>Formula 1 2013 Results</td>
                             </tr>
-                            <tr>
+                            <tr className="raceTable-headerDown">
                                 <td>Round</td>
                                 <td>Grand Prix</td>
                                 <td>Team</td>
