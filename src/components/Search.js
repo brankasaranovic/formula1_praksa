e2b1c30c3df5fe28590590c143e5334c84b2a0ed
Branka <saranovicbranka@gmail.com>
2022-07-07 14:43:40 +0200

    Komponenta search

---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Search extends React.Component {
+    render() {
+        return (
+            <input
+                placeholder="Search"
+                defaultValue={this.props.filterValue}
+                onChange={(e) => this.props.handleFilter(e)}>
+            </input>
+        );
+    }
+
+}
